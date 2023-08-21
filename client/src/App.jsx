978edb6ea53b69aab4ef84b70f714053baad97d5
Aiganym <ospan.aiganym@gmail.com>
2023-08-21 23:23:34 +0600

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { ROUTES } from "./routes/routesConfig";
+import MainPage from "./pages/MainPage";
+import { Provider } from "react-redux";
+import store from "./stateManagement/store";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path={ROUTES.MAIN} element={<ProtectedRoutes />}>
+            <Route path={ROUTES.MAIN} element={<MainPage />} />
+          </Route>
+
+          <Route path={ROUTES.REGISTER} element={<Register />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+          <Route path="*" element={<Navigate to={ROUTES.NOTFOUND} />} />
+          <Route path={ROUTES.NOTFOUND} element={<PageNotFound />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
